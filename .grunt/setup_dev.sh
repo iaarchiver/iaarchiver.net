311c998a9d36e24173c66ce401659601ff46f4d1
iaarchiver <ia.archiver@gmail.com>
2013-06-16 09:00:29 +0900

    [grunt] setup_dev.shを作成

---
+++ .grunt/setup_dev.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# setup 
+#
+#
+
+# 
+SITEURL='http://iaarchiver.net'
+DIR='html'
+
+wget -r -np $SITEURL -N -nH -P $DIR
+
+if [[ ! -d $DIR]];then
+	exit
+fi
+
+# replace a pattern(PTR) in all files of target(DIR)
+PTR1='s/http:\/\/s3\.iaarchiver\.net\//\.\//g'
+PTR2='s/http:\/\/iaarchiver\.net\//\.\//g'
+PTR3='s/<\/body>/<script src=\"http:\/\/localhost:35729\/livereload.js\"><\/script><\/body>/g'
+
+IFS='
+'
+cd $DIR
+find . -follow -type f -name "*" | while read file; do
+	echo "replacing $file ..."
+	sed -e $PTR1 "$file" > "$file.tmp0"
+	sed -e $PTR2 "$file.tmp0" > "$file.tmp1"
+	sed -e $PTR3 "$file.tmp1" > "$file"
+	rm "$file.tmp0"
+	rm "$file.tmp1"
+done
+cd ..
+
+# generate symlink html/index.html to ../index.html
+ABSDIR=$(cd $DIR && pwd)
+ln -s "$ABSDIR/../../style" "$ABSDIR/" 
+ln -s "$ABSDIR/../../scripts" "$ABSDIR/" 
