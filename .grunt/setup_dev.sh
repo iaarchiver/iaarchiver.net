268b5aea97218e0225fc4be7ced72a10f4f49c49
iaarchiver <ia.archiver@gmail.com>
2014-04-26 18:10:46 +0900

    htmlファイル同期用スクリプトを調整

--- .grunt/setup_dev.sh
+++ .grunt/setup_dev.sh
@@ -1,37 +1,16 @@
 #!/bin/sh
-# setup 
-#
-#
+# Sync local html-files for live-reload
 
-# 
 SITEURL='http://iaarchiver.net'
 DIR='html'
 
+# if $DIR doesn't exist, exit
+[[ -d $DIR ]] || exit
+
+# download html-files to $DIR
 wget -r -np $SITEURL -N -nH -P $DIR
 
-if [[ ! -d $DIR ]];then
-	exit
-fi
-
-# replace a pattern(PTR) in all files of target(DIR)
-#PTR1='s/http:\/\/s3\.iaarchiver\.net/http:\/\/localhost\:3000/g'
-#PTR2='s/http:\/\/iaarchiver\.net//g'
-#PTR3='s/<\/body>/<script src=\"http:\/\/localhost:35729\/livereload.js\"><\/script><\/body>/g'
-
-#IFS='
-#'
-#cd $DIR
-#find . -type f -name "*" | while read file; do
-#	echo "replacing $file ..."
-#	sed -e $PTR1 "$file" > "$file.tmp0"
-#	sed -e $PTR2 "$file.tmp0" > "$file.tmp1"
-#	sed -e $PTR3 "$file.tmp1" > "$file"
-#	rm "$file.tmp0"
-#	rm "$file.tmp1"
-#done
-#cd ..
-
-# generate symlink html/index.html to ../index.html
+# generate symlink ./style & ./scripts
 ABSDIR=$(cd $DIR && pwd)
 ln -s "$ABSDIR/../../style" "$ABSDIR/" 
 ln -s "$ABSDIR/../../scripts" "$ABSDIR/" 
