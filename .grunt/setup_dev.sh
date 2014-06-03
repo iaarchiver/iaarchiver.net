ddbfb3880936ce68a97c911c3a1300332a409a9a
iaarchiver <ia.archiver@gmail.com>
2014-06-03 22:48:08 +0900

    setup_dev.shを更新．ローカルのstyle.css/all.jsをロードさせる．

--- .grunt/setup_dev.sh
+++ .grunt/setup_dev.sh
@@ -1,16 +1,25 @@
-#!/bin/sh
-# Sync local html-files for live-reload
+#!/bin/bash
 
 SITEURL='http://iaarchiver.net'
 DIR='html'
 
-# if $DIR doesn't exist, exit
-[[ -d $DIR ]] || exit
+### if $DIR doesn't exist, exit
+[[ -d $DIR ]] || { echo "setup failed."; exit 1; }
 
-# download html-files to $DIR
+### download html-files to $DIR
 wget -r -np $SITEURL -N -nH -P $DIR
 
-# generate symlink ./style & ./scripts
-ABSDIR=$(cd $DIR && pwd)
-ln -s "$ABSDIR/../../style" "$ABSDIR/" 
-ln -s "$ABSDIR/../../scripts" "$ABSDIR/" 
+### replace a pattern(PTR) in all files of target(DIR)
+PTR1='s/http:\/\/s3\.iaarchiver\.net/http:\/\/localhost\:3000/g'
+PTR2='s/http:\/\/iaarchiver\.net//g'
+
+find $DIR -type f -name "*" | while read file; do
+	echo "replacing $file ... "
+	sed -i '' -e $PTR1 "$file"
+	sed -i '' -e $PTR2 "$file"
+done
+
+### generate symlink ./style & ./scripts
+ABSDIR=$(cd $DIR; pwd)
+ln -s "$ABSDIR/../../style" "$ABSDIR/"
+ln -s "$ABSDIR/../../scripts" "$ABSDIR/"
